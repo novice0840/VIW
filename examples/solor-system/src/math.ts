01e57dcfb8cff625c7418274fc27a05b1cd7d132
novice0840 <magnolia0840@gmail.com>
2026-03-09 00:58:47 +0900

    docs: lookAt 함수 학습 완료

--- examples/solor-system/src/math.ts
+++ examples/solor-system/src/math.ts
@@ -4,8 +4,10 @@ export type Mat4 = Float32Array;
 export type Vec3 = [number, number, number];
 
 export const mat4 = {
+  /**
+   * @description 단위 행렬을 반환하는 함수
+   */
   identity(): Mat4 {
-    // 단위 행렬을 반환하는 함수
     const m = new Float32Array(16);
     m[0] = 1;
     m[5] = 1;
@@ -14,6 +16,9 @@ export const mat4 = {
     return m;
   },
 
+  /**
+   * @description 원근 행렬을 반환하는 함수
+   */
   perspective(fov: number, aspect: number, near: number, far: number): Mat4 {
     const m = new Float32Array(16);
     const f = 1.0 / Math.tan(fov / 2);
@@ -26,19 +31,30 @@ export const mat4 = {
     return m;
   },
 
+  /**
+   * @description 카메라가 특정 지점을 바라보도록 하는 뷰(view) 행렬을 만드는 함수
+   * @param eye 카메라 위치
+   * @param center 카메라가 바라오는 대상 위치
+   * @param up 위쪽 방향 (보통 [0, 1, 0])
+   * @returns view 행렬
+   */
   lookAt(eye: Vec3, center: Vec3, up: Vec3): Mat4 {
     const zx = eye[0] - center[0];
     const zy = eye[1] - center[1];
     const zz = eye[2] - center[2];
+    // len = 1 / 벡터 길이
     let len = 1 / Math.sqrt(zx * zx + zy * zy + zz * zz);
+    // fz: 중점 -> 카메라를 향하는 방향 벡터를 정규화한 단위 벡터
     const fz: Vec3 = [zx * len, zy * len, zz * len];
 
     const xx = up[1] * fz[2] - up[2] * fz[1];
     const xy = up[2] * fz[0] - up[0] * fz[2];
     const xz = up[0] * fz[1] - up[1] * fz[0];
     len = 1 / Math.sqrt(xx * xx + xy * xy + xz * xz);
+    // fx: up, fz의 외적 벡터를 정규화한 단위 벡터
     const fx: Vec3 = [xx * len, xy * len, xz * len];
 
+    // fy: fx의 반대방향에 있는 단위 벡터
     const fy: Vec3 = [
       fz[1] * fx[2] - fz[2] * fx[1],
       fz[2] * fx[0] - fz[0] * fx[2],
@@ -58,6 +74,7 @@ export const mat4 = {
     m[9] = fy[2];
     m[10] = fz[2];
     m[11] = 0;
+    // f*와 eye 벡터의 내적의 반대 부호 값
     m[12] = -(fx[0] * eye[0] + fx[1] * eye[1] + fx[2] * eye[2]);
     m[13] = -(fy[0] * eye[0] + fy[1] * eye[1] + fy[2] * eye[2]);
     m[14] = -(fz[0] * eye[0] + fz[1] * eye[1] + fz[2] * eye[2]);
